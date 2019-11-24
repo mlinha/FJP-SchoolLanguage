df9c10da941e5a267a94e32a1402e00a2436bcd3
mlinha <mlinhac@gmail.com>
2019-11-24 17:36:09 +0100

    Initial commit

---
+++ src/syntax/RecursiveDescentParser.java
@@ -0,0 +1,74 @@
+package syntax;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class RecursiveDescentParser {
+    private Iterator<String> iterator;
+    private String symbol;
+
+    public  RecursiveDescentParser(List<String> data) {
+        iterator = data.iterator();
+    }
+
+    private void getNextSymbol() {
+        if (iterator.hasNext()) {
+            symbol = iterator.next();
+        }
+        symbol = null;
+    }
+
+    private void verify(String symbol, String with) {
+        if(symbol.equals(with)) {
+            getNextSymbol();
+        }
+    }
+
+    public void program() {
+        getNextSymbol();
+        globPromenne();
+        funkceProcedury();
+    }
+
+    private void globPromenne() {
+        while(!symbol.equals("procedura") && !symbol.equals("funkce")) {
+            if(symbol.equals("konst")) {
+                getNextSymbol();
+            }
+            verify(symbol, "ident");
+            verify(symbol, "=");
+            if(symbol.equals("hodnota")) {
+                verify(symbol, "hodnota");
+            }
+            else if(symbol.equals("volani funkce")) {
+                verify(symbol, "volani funkce");
+            }
+            else {
+                //error
+            }
+            verify(symbol, ";");
+        }
+    }
+
+    private void funkceProcedury() {
+        while(symbol.equals("funkce")) {
+            getNextSymbol();
+            verify(symbol, "navratova hodnota");
+            verify(symbol, "ident");
+            verify(symbol, "(");
+            parametry();
+            verify(symbol, ")");
+            verify(symbol, "{");
+            vnitrekFunkce();
+            verify(symbol, "}");
+        }
+    }
+
+    private void parametry() {
+
+    }
+
+    private void vnitrekFunkce() {
+
+    }
+}
